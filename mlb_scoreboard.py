91bcd806a43420ca3aaebfa26919e91a8f909f39
Jason <jasahamm@gmail.com>
2016-09-12 19:50:17 -0400

    Added in game progress

--- mlb_scoreboard.py
+++ mlb_scoreboard.py
@@ -40,7 +40,19 @@ teamColorDict['Rockies'] = ['grey', 'on_magenta']
 teamColorDict['Astros'] = ['white', 'on_red']
 teamColorDict['Cubs'] = ['red', 'on_blue']
 
+banner1 = "#   ____    ____  _____     ______      ______     ______    ___   _______     ________  ______      ___        _       _______     ______"
+banner2 = "#  |_   \  /   _||_   _|   |_   _ \   .' ____ \  .' ___  | .'   `.|_   __ \   |_   __  ||_   _ \   .'   `.     / \     |_   __ \   |_   _ `."
+banner3 = "#    |   \/   |    | |       | |_) |  | (___ \_|/ .'   \_|/  .-.  \ | |__) |    | |_ \_|  | |_) | /  .-.  \   / _ \      | |__) |    | | `. \ "
+banner4 = "#    | |\  /| |    | |   _   |  __'.   _.____`. | |       | |   | | |  __ /     |  _| _   |  __'. | |   | |  / ___ \     |  __ /     | |  | | "
+banner5 = "#   _| |_\/_| |_  _| |__/ | _| |__) | | \____) |\ `.___.'\\\  `-'  /_| |  \ \_  _| |__/ | _| |__) |\  `-'  /_/ /   \ \_  _| |  \ \_  _| |_.' / "
+banner6 = "#  |_____||_____||________||_______/   \______.' `.____ .' `.___.'|____| |___||________||_______/  `.___.'|____| |____||____| |___||______.'  "
 
+print(banner1)
+print(banner2)
+print(banner3)
+print(banner4)
+print(banner5)
+print(banner6)
 #print(str(len(sys.argv)))
 # Must be either 0 or 3 command line arguments
 if ((len(sys.argv) != 1) and (len(sys.argv) != 4)):
@@ -86,42 +98,104 @@ try:
 	numberOfFinishedGames = len(scoreboard['go_game'])
 except Exception as e:
 	#print(e)
-	print("No Completed Games")
+	print("No Completed Games\n")
 	numberOfFinishedGames = 0
 
 try:
 	numberOfInProgressGames = len(scoreboard['ig_game'])
 except Exception:
-	print("No In Progress Games")
+	print("No In Progress Games\n")
 	numberOfInProgressGames = 0
 
 
 '''
 '''
-def get_games(scoreboard, gameType):
-	#print("")
-	print("Team\t\t\tRuns\tHits\tErrors\n")
-	for game in scoreboard[gameType]:
-		for team in game['team']:
-			runs = team['gameteam']['@R']
-			hits = team['gameteam']['@H']
-			errors = team['gameteam']['@E']
-			name = team['@name']
-			spaces = " " * (10 - len(name))
-			#cprint(name, teamColorDict[name][0], teamColorDict[name][1], end="")
-			print(name, end="")
-			print(spaces + "\t\t" + str(runs) + "\t" + str(hits) + "\t" + str(errors))
-		print()
+def get_games(scoreboard):
+
+	
+	gameType = 'go_game'
+	if (numberOfFinishedGames > 0):
+		print("Team\t\t\tRuns\tHits\tErrors\n")
+		for game in scoreboard[gameType]:
+			for team in game['team']:
+				runs = team['gameteam']['@R']
+				hits = team['gameteam']['@H']
+				errors = team['gameteam']['@E']
+				name = team['@name']
+				spaces = " " * (10 - len(name))
+				#cprint(name, teamColorDict[name][0], teamColorDict[name][1], end="")
+				print(name, end="")
+				print(spaces + "\t\t" + str(runs) + "\t" + str(hits) + "\t" + str(errors))
+			print()
+	gameType = 'ig_game'
+	if (numberOfInProgressGames > 0):
+		print("Team\t\t\tRuns\tHits\tErrors")
+		for game in scoreboard[gameType]:
+			#print(game.keys())
+			print()
+			status = game['game']['@status']
+			startTime = game['game']['@start_time']
+			#print(game['game'])
+			if (status == "PRE_GAME" or status == "IMMEDIATE_PREGAME"):
+				print("PREGAME\nStart Time = ", end="")
+				print(startTime)
+				for team in game['team']:
+					runs = team['gameteam']['@R']
+					hits = team['gameteam']['@H']
+					errors = team['gameteam']['@E']
+					name = team['@name']
+					spaces = " " * (10 - len(name))
+					#cprint(name, teamColorDict[name][0], teamColorDict[name][1], end="")
+					print(name, end="")
+					print(spaces + "\t\t" + str(runs) + "\t" + str(hits) + "\t" + str(errors))
+			elif status == "IN_PROGRESS":
+				inning = game['inningnum']
+				outs = game['@outs']
+				
+				for team in game['team']:
+					runs = team['gameteam']['@R']
+					hits = team['gameteam']['@H']
+					errors = team['gameteam']['@E']
+					name = team['@name']
+					spaces = " " * (10 - len(name))
+					#cprint(name, teamColorDict[name][0], teamColorDict[name][1], end="")
+					print(name, end="")
+					print(spaces + "\t\t" + str(runs) + "\t" + str(hits) + "\t" + str(errors))
+				print(inning['@half'] + str(inning['@inning']) + " " + str(outs) + " outs")
+				try:
+					pitcher = game['pitcher']
+					print("P: " + pitcher['@name'])
+				except KeyError as k:
+					pass
+				try:
+					batter = game['batter']
+					print("AB: " + batter['@name'])
+				except KeyError as k:
+					pass
+				try:
+					on_base = game['on_base']
+					print("On Base: ")
+					#print(len(game['on_base']))
+					try:
+						for base_runner in on_base:
+							#print(base_runner)
+							base_number = base_runner['@base']
+							player = base_runner['player']
+							print(str(base_number) + ": " + player['@name'])
+					except TypeError as t:
+						base_number = on_base['@base']
+						player = on_base['player']
+						print(str(base_number) + ": " + player['@name'])
+					
+				except KeyError as k:
+					pass
+			
 	return (True)
 
 
 
 
-if (numberOfFinishedGames > 0):
-	print("\nCompleted Games for " + str(month) + "/" + str(day) + "/" + str(year))
-	print()
-	get_games(scoreboard, 'go_game')
-
+get_games(scoreboard)
 
 
 '''
